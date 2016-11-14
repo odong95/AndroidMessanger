83466af3bea7405a434182c17d610ba09ab07aa4
odong95 <odong951@gmail.com>
2016-11-14 02:48:12 -0600

    conversation

--- WaspMessenger/app/src/main/java/com/waspteam/waspmessenger/ConversationActivity.java
+++ WaspMessenger/app/src/main/java/com/waspteam/waspmessenger/ConversationActivity.java
@@ -16,13 +16,15 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-
+import android.graphics.Color;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.security.SecureRandom;
 
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
@@ -47,6 +49,7 @@ import com.microsoft.windowsazure.mobileservices.table.sync.synchandler.SimpleSy
 import com.squareup.okhttp.OkHttpClient;
 import java.net.MalformedURLException;
 import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.*;
+import static com.waspteam.waspmessenger.R.string.generate_handle;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
@@ -61,7 +64,7 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-
+import android.content.DialogInterface;
 import com.microsoft.windowsazure.mobileservices.MobileServiceClient;
 import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
 
@@ -73,11 +76,14 @@ public class ConversationActivity extends AppCompatActivity
     private EditText mConversationName, mConversationCode;
     private MobileServiceClient mClient;
     private MobileServiceTable<Conversation> mConvoTable;
+    private MobileServiceTable<User> mUserTable;
     private String mUsername;
     //DUMMY HANDLE CODE
     private String mHandle = "XXX";
     ConversationAdapter mAdapter;
 
+    static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    static SecureRandom rnd = new SecureRandom();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,13 +91,13 @@ public class ConversationActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         mConversationName = (EditText) findViewById(R.id.newConversationNickname);
         mConversationCode = (EditText) findViewById(R.id.newConversationHandleCode);
 
         //Gets String passed the login activity
         Intent intent = getIntent();
         mUsername= intent.getStringExtra("username");
-       // mUsername = "Julian";
 
         mAdapter = new ConversationAdapter(this, R.layout.row_conversation);
         ListView listViewConversation = (ListView) findViewById(R.id.listView_conversation);
@@ -105,7 +111,7 @@ public class ConversationActivity extends AppCompatActivity
                     "https://waspsmessenger.azurewebsites.net",
                     this);
             mConvoTable = mClient.getTable(Conversation.class);
-
+            mUserTable = mClient.getTable(User.class);
             mClient.setAndroidHttpClientFactory(new OkHttpClientFactory() {
                 @Override
                 public OkHttpClient createOkHttpClient() {
@@ -116,6 +122,7 @@ public class ConversationActivity extends AppCompatActivity
                 }
             });
 
+            refreshItemsFromTable();
 
         }
         catch (MalformedURLException e)
@@ -148,7 +155,6 @@ public class ConversationActivity extends AppCompatActivity
                     else
                     {
                         addConversation(view);
-
                         mConversationName.setText("");
                         mConversationCode.setText("");
                         editLayout.setVisibility(LinearLayout.GONE);
@@ -157,7 +163,7 @@ public class ConversationActivity extends AppCompatActivity
                 }
             }
         });
-        refreshItemsFromTable();
+
 
     }
 
@@ -176,12 +182,72 @@ public class ConversationActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.generate_handle) {
+            handleGeneration();
+        }
+        else if(id == R.id.view_handle)
+        {
+            final Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Handle Code: " + mHandle, Snackbar.LENGTH_LONG);
+                    snackbar.setAction("Close", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            snackbar.dismiss();
+                        }
+                    })
+                    .setActionTextColor(Color.WHITE).setDuration(100000).show();
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void handleGeneration()
+    {
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>()
+        {
+            protected Void doInBackground(Void... objects) {
+                try {
+                    final List<User> user = mUserTable.where().field("username").eq(mUsername).execute().get();
+
+                    final User u = user.get(0);
+                    final List<Conversation> convosA = userConversations(mHandle);
+                    final List<Conversation> convosB = userConversationsB(mHandle);
+                    System.out.println("size " + convosA.size());
+                    runOnUiThread(new Runnable(){
+                        @Override
+                        public void run() {
+
+                            AlertDialog.Builder builder = new AlertDialog.Builder(ConversationActivity.this);
+                            String newHandle = generateHandle(10);
+                            u.handle = newHandle;
+                            mUserTable.update(u);
+
+                            for (Conversation item : convosA) {
+                                item.mHandleA = newHandle;
+                                mConvoTable.update(item);
+                            }
+                            for (Conversation item2 : convosB) {
+                                item2.mHandleB = newHandle;
+                                mConvoTable.update(item2);
+                            }
+                            final Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "New Handle Code: " + newHandle, Snackbar.LENGTH_LONG);
+                            snackbar.setAction("Close", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    snackbar.dismiss();
+                                }
+                            })
+                                    .setActionTextColor(Color.WHITE).setDuration(100000).show();
+                        }
+                    });
+                } catch (final Exception e){
+                    createAndShowDialogFromTask(e, "Error");
+                }
+
+                return null;
+            }
+        }.execute();
+    }
+
 
     public void addConversation(View view)
     {
@@ -197,13 +263,7 @@ public class ConversationActivity extends AppCompatActivity
         }
     }
 
-    private boolean validInput(android.text.Editable field)
-    {
-        //Peculiarly, isEmpty returns true when the field is filled
-        return TextUtils.isEmpty(field);
-    }
 
-    //checks if conversation already exists
     private void addToTable(final Conversation addConversation, final String handle, final String nick)
     {
         AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
@@ -211,37 +271,36 @@ public class ConversationActivity extends AppCompatActivity
             protected Void doInBackground(Void... params) {
 
                 try {
-                    final List<Conversation> results = existedConversation(handle);
-                    if(results.size()> 0) {
-                        //System.out.println("Conversation exists, updating table");
-                        final Conversation c = results.get(0);
-                        runOnUiThread(new Runnable() {
+                    final List<User> results = checkUser(handle);
+
+                    if (results.size()>0) {
+                        runOnUiThread(new Runnable(){
                             @Override
                             public void run() {
+                                final User u = results.get(0);
                                 mAdapter.clear();
-                                c.mNicknameB = nick;
-                                String temp = c.mHandleB;
-                                c.mHandleB = c.mHandleA;
-                                c.mHandleA = temp;
-                                mConvoTable.update(c);
-                                mAdapter.add(c);
-                            }
-                        });
-                    }else
-                    {
-                        //System.out.println("Adding new conversation to table");
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
                                 addConversation.mHandleB = handle;
-                                addConversation.mHandleA = mUsername;
+                                addConversation.mHandleA = mHandle;
                                 addConversation.mNicknameA = nick;
+                                addConversation.mNicknameB = u.username;
                                 mConvoTable.insert(addConversation);
                                 mAdapter.add(addConversation);
+                                refreshItemsFromTable();
                             }
                         });
 
-                    }
+                    } else
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(ConversationActivity.this);
+                                builder.setMessage("Invalid Handle ID")
+                                        .setNegativeButton("Try again", null)
+                                        .create().show();
+                            }
+                        });
+
+                    return null;
 
 
                 } catch (final Exception e){
@@ -254,10 +313,66 @@ public class ConversationActivity extends AppCompatActivity
         runAsyncTask(task);
     }
 
-    private List<Conversation> existedConversation(String h) throws ExecutionException, InterruptedException
-    {
-        return mConvoTable.where().field("handleB").eq(val(mUsername)).and().field("handleA").eq(val(h)).execute().get();
+    public void refreshItemsFromTable() {
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... params) {
+
+                try {
+                    final List<User> user = mUserTable.where().field("username").eq(mUsername).execute().get();
+                    final User u = user.get(0);
+                    mHandle = u.handle;
+                    final List<Conversation> results = refreshItemsFromConvoTable();
+                    final List<Conversation> results2 = refreshItemsFromConvoTable2();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            mAdapter.clear();
+                            for (Conversation item : results) {
+                                item.isExist = true;
+                                mAdapter.add(item);
+                            }
+                            for (Conversation item2 : results2) {
+                                item2.isExist = false;
+                                mAdapter.add(item2);
+                            }
+                        }
+                    });
+                } catch (final Exception e){
+                    createAndShowDialogFromTask(e, "Error");
+                }
+
+                return null;
+            }
+        };
+
+        runAsyncTask(task);
     }
+
+    private List<Conversation> refreshItemsFromConvoTable() throws ExecutionException, InterruptedException {
+        return mConvoTable.where().field("handleA").eq(val(mHandle)).execute().get();
+    }
+
+    private List<Conversation> refreshItemsFromConvoTable2() throws ExecutionException, InterruptedException {
+        return mConvoTable.where().field("handleB").eq(val(mHandle)).execute().get();
+    }
+
+    private List<User> checkUser(String h) throws ExecutionException, InterruptedException
+    {
+        return mUserTable.where().field("handle").eq(val(h)).execute().get();
+    }
+
+    private List<Conversation> userConversations(String h) throws ExecutionException, InterruptedException
+    {
+        return mConvoTable.where().field("handleA").eq(val(h)).execute().get();
+    }
+
+    private List<Conversation> userConversationsB(String h) throws ExecutionException, InterruptedException
+    {
+        return mConvoTable.where().field("handleB").eq(val(h)).execute().get();
+    }
+
     private void createAndShowDialog(Exception exception, String title) {
         Throwable ex = exception;
         if(exception.getCause() != null){
@@ -291,56 +406,46 @@ public class ConversationActivity extends AppCompatActivity
         }
     }
 
-    public void refreshItemsFromTable() {
-        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
-            @Override
-            protected Void doInBackground(Void... params) {
-
-                try {
-                    final List<Conversation> results = refreshItemsFromConvoTable();
-
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            mAdapter.clear();
-
-                            for (Conversation item : results) {
-                                mAdapter.add(item);
-                            }
-                        }
-                    });
-                } catch (final Exception e){
-                    createAndShowDialogFromTask(e, "Error");
-                }
-
-                return null;
-            }
-        };
-
-        runAsyncTask(task);
+    private boolean validInput(android.text.Editable field)
+    {
+        //Peculiarly, isEmpty returns true when the field is filled
+        return TextUtils.isEmpty(field);
     }
 
-    private List<Conversation> refreshItemsFromConvoTable() throws ExecutionException, InterruptedException {
-        return mConvoTable.where().field("handleA").
-                eq(val(mUsername)).or().field("handleB").eq(val(mUsername)).execute().get();
-    }
 
-    public void startMessaging(View view, String myNewNick, String toHandle)
+
+    public void startMessaging(View view, Conversation c)
     {
         Intent intent = new Intent(view.getContext(), MessagingActivity.class);
         Bundle bundle = new Bundle();
         bundle.putString("EXTRA_MYUSERNAME",mUsername);
         bundle.putString("EXTRA_MYHANDLE",mHandle);
-        bundle.putString("EXTRA_MYNICKNAME",myNewNick);
-        bundle.putString("EXTRA_TOHANDLE",toHandle);
+        if(c.isExist) {
+            bundle.putString("EXTRA_MYNICKNAME", c.getNicknameA());
+            bundle.putString("EXTRA_TOHANDLE",c.mHandleB);
+            bundle.putString("EXTRA_TONICK",c.getNicknameB());
+        }
+        else
+        {
+            bundle.putString("EXTRA_MYNICKNAME", c.getNicknameB());
+            bundle.putString("EXTRA_TOHANDLE",c.mHandleA);
+            bundle.putString("EXTRA_TONICK",c.getNicknameA());
+        }
         //TONICK must be established by a call to the database
         //It won't even exist until the other party accepts, so this logic will require adjusting
-        bundle.putString("EXTRA_TONICK",toHandle);
+
         intent.putExtras(bundle);
         view.getContext().startActivity(intent);
     }
 
+    private String generateHandle(int n){
+
+        StringBuilder sb = new StringBuilder( n );
+        sb.append('$');
+        for( int i = 0; i < n-1; i++ )
+            sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
+        return sb.toString();
+    }
 }
 
 
